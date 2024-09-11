2947bac6c2f71e30781883368111a7bd93c7852d
Ondřej Častoral <ondrejcastoral@email.cz>
2024-09-11 19:21:53 +0200

    init commit

---
+++ src/test/java/com/castixz/demo_security_app/DemoSecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castixz.demo_security_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSecurityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
